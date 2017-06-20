d7fd481ad8f534fc314af13752e94be6974b0969
BrianSheridan <briansheridan732@gmail.com>
2017-06-20 10:08:37 +0100

    Django first blog commit

--- blog_prj/settings.py
+++ blog_prj/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'django.contrib.sites',  
+    'disqus',
+    'django_forms_bootstrap',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -119,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+DISQUS_WEBSITE_SHORTNAME = 'BrianSheridan'
+SITE_ID = 1
